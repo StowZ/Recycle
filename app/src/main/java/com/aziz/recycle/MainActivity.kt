cfb6551f70d14cacd9bd561bc77e5948c2e2b369
Aziz <naftiazizsteam@hotmail.fr>
2021-11-06 13:29:50 +0100

    Final commit.

--- app/src/main/java/com/aziz/recycle/MainActivity.kt
+++ app/src/main/java/com/aziz/recycle/MainActivity.kt
@@ -2,6 +2,7 @@ package com.aziz.recycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aziz.recycle.adapter.ItemAdapter
 import com.aziz.recycle.data.Datasource
@@ -16,9 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+
 
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
+
     }
 }
